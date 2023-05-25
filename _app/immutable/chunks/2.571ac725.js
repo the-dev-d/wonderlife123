2f4c528ccbe85b622c1475241e1e72b231e00610
Dhaivath <dhaivathl@gmail.com>
2023-05-25 13:12:57 +0530

    Updates

---
+++ _app/immutable/chunks/2.571ac725.js
@@ -0,0 +1,1 @@
+import{S as n,i as r,s as l,q as c,r as i,b as m,H as o,h as p}from"./index.a7df7965.js";function f(a){let t;return{c(){t=c("Hello from root page")},l(e){t=i(e,"Hello from root page")},m(e,s){m(e,t,s)},p:o,i:o,o,d(e){e&&p(t)}}}class u extends n{constructor(t){super(),r(this,t,null,f,l,{})}}const x=u;export{x as component};
